f335f8e351cc91460c974f79019d4e2abfdaea85
EageRAssassin <rt389@cornell.edu>
2018-10-27 14:55:09 -0400

    buffer program works!!!!

---
+++ buffer_program/buffer.py
@@ -0,0 +1,34 @@
+import socket
+import sys
+
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+	print('Failed to create socket')
+	sys.exit()
+print('Socket Created')
+
+host = '192.168.4.148'
+port = 6000
+
+try:
+    remote_ip = socket.gethostbyname( host )
+except socket.gaierror:
+	print('Hostname could not be resolved. Exiting')
+	sys.exit()
+
+#Connect to remote server
+s.connect((remote_ip , port))
+print('Socket Connected to ' + host + ' on ip ' + remote_ip)
+
+
+filename = open('camerashot.jpg', 'wb')
+while True:
+    strng = s.recv(1024)
+    if not strng:
+        break
+    filename.write(strng)
+filename.close()
+print('received, yay!')
+
+s.close()
