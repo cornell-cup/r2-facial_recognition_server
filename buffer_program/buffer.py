d403e7eb4ce4d2136933509e13f1146f7a780706
WendyHuang <bh486@cornell.edu>
2018-11-03 13:41:18 -0400

    update the buffer program and facial_recognition part
    add json parsing and simple check in status check

--- buffer_program/buffer.py
+++ buffer_program/buffer.py
@@ -1,34 +1,81 @@
 import socket
 import sys
+import json
+import datetime
+from buffer_program.facial_recognition import parse_json
 
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-	print('Failed to create socket')
-	sys.exit()
-print('Socket Created')
+#get image from the Rasberry Pi via socket
+def getImage():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print('Failed to create socket')
+        sys.exit()
+    print('Socket Created')
 
-host = '192.168.4.148'
-port = 6000
+    host = '192.168.4.148'
+    port = 6000
 
-try:
-    remote_ip = socket.gethostbyname( host )
-except socket.gaierror:
-	print('Hostname could not be resolved. Exiting')
-	sys.exit()
+    try:
+        remote_ip = socket.gethostbyname( host )
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
 
-#Connect to remote server
-s.connect((remote_ip , port))
-print('Socket Connected to ' + host + ' on ip ' + remote_ip)
+    #Connect to remote server
+    s.connect((remote_ip , port))
+    print('Socket Connected to ' + host + ' on ip ' + remote_ip)
 
 
-filename = open('camerashot.jpg', 'wb')
-while True:
-    strng = s.recv(1024)
-    if not strng:
-        break
-    filename.write(strng)
-filename.close()
-print('received, yay!')
+    filename = open('camerashot.jpg', 'wb')
+    while True:
+        strng = s.recv(1024)
+        if not strng:
+            break
+        filename.write(strng)
 
-s.close()
+    filename.close()
+    print('received, yay!')
+
+    s.close()
+
+#Check the attendance of person with the name and return a complete check in status in json format
+def checkAttendance(inputNameJson):
+
+    text = json.loads(inputNameJson)
+
+    #Name
+    face_name = text[name]
+
+    #timeCheck
+    nowTime = datetime.datetime.now()
+    today12pm = datetime.time(hour=12)
+
+    #Status(need to update)
+    statusOnTime = nowTime <= today12pm
+
+    #MeetingType(need to update)
+    meetingType = "Saturday Work Meeting"
+
+    return parseToJson(face_name, nowTime, statusOnTime, meetingType)
+
+#Parse the Check in result to json
+def parseToJson(face_name, nowTime, statusOnTime, meetingType):
+    statusInString = ""
+    if statusOnTime:
+        status = "OnTime"
+    else:
+        status = "Late"
+
+        CheckInData = {
+        'name': face_name,
+        'CurrentTime': nowTime,
+        'Status': status,
+        'MeetingType': meetingType
+    }
+
+    jsonCheckInData = json.dumps(CheckInData)
+
+    return jsonCheckInData
+
+
