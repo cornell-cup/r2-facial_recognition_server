9aa1e32a917e0fa14affc4d060f5642344804d20
WendyHuang <bh486@cornell.edu>
2019-03-09 15:56:29 -0500

    database

---
+++ util/database.py
@@ -0,0 +1,65 @@
+import json
+meeting = {
+	"R2":[
+		{
+			"name" : "SaturdayMeeting",
+			"time": 12,
+			"dayofWeek": "Saturday"
+		},
+		{
+			"name": "Dave meeting",
+			"time": 17,
+			"dayofWeek": "Friday"
+		},
+		{
+			"name": "Weekly work meeting",
+			"time": 17,
+			"dayofWeek": "Wednesday"
+		},
+	],
+	"BuddyBot":[
+		{
+			"name" : "SaturdayMeeting",
+			"time": 12,
+			"dayofWeek": "Saturday"
+		},
+		{
+			"name": "Dave meeting",
+			"time": 17,
+			"dayofWeek": "Friday"
+		},
+		{
+			"name": "Weekly work meeting",
+			"time": 17,
+			"dayofWeek": "Wednesday"
+		},
+	],
+	"Communication": [
+		{
+			"name": "SaturdayMeeting",
+			"time": 12,
+			"dayofWeek": "Saturday"
+		},
+		{
+			"name": "Dave meeting",
+			"time": 17,
+			"dayofWeek": "Friday"
+		},
+		{
+			"name": "Weekly work meeting",
+			"time": 17,
+			"dayofWeek": "Wednesday"
+		},
+	]
+
+}
+
+
+Member = {
+    "R2":["Ian", "Jordan","David","Stanley","Charles","Varun","Yanchen","Zhen","Renee","Angela"
+          ,"Wendy","Jackie","Yuanxin","Scott","Fareeza" ,"Charu","Bryce","Emily","Dylan","Tobias"],
+    "Communication":[],
+    "Buddybot":["Rishi", "Kathy"],
+    "Minibot":["Marcus", "Ananth", "Stefen", "Dongqing" , "Zoltan", "Kabir", "Dakota","Alberto",
+               "Shiang-Wan","Sophie","Shubham","Richard"]
+}
